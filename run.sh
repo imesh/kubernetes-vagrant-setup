67a944845e10b91f74fb5539b2c75cb54eaee691
Imesh Gunaratne <imesh@apache.org>
2016-03-25 10:39:38 +0530

    Adding logic to download docker images

--- run.sh
+++ run.sh
@@ -22,21 +22,80 @@
 set -e
 
 SECONDS=0
-#echo "Starting http server at http://localhost:8000"
-#pushd bin/kubernetes/
-#python http-server.py > /dev/null 2>&1 &
-#popd
+docker_image_dir="bin/kubernetes/v1.2.0/docker/images/"
 
-#echo "Starting vagrant setup..."
+function echoError () {
+    echo $'\e[1;31m'"${1}"$'\e[0m'
+}
+
+function echoSuccess () {
+    echo $'\e[1;32m'"${1}"$'\e[0m'
+}
+
+function echoBold () {
+    echo $'\e[1m'"${1}"$'\e[0m'
+}
+
+function docker_pull() {
+  image_tag=$1
+  echoBold "--> docker pull ${image_tag}"
+  docker pull ${image_tag}
+}
+
+function docker_save() {
+  image_tag=$1
+  file_name=$2
+  echoBold "--> docker save ${image_tag} > ${file_name}"
+  docker save ${image_tag} > ${file_name}
+}
+
+if [ "$(ls ${docker_image_dir})" ]; then
+  echoBold "Docker image files found in ${docker_image_dir}"
+else
+  echoBold "Docker image files not found in ${docker_image_dir}"
+  echoBold "Trying to pull docker images, this is a one time operation..."
+
+  if ! [ -x "$(command -v docker)" ]; then
+    echoError 'Docker is not installed, existing...' >&2
+    exit 1
+  fi
+
+  docker_pull quay.io/devops/docker-registry:latest
+  docker_pull gcr.io/google_containers/etcd:2.0.9
+  docker_pull gcr.io/google_containers/kube-registry-proxy:0.3
+  docker_pull gcr.io/google_containers/pause:2.0
+  docker_pull quay.io/coreos/flannel:0.5.5
+  docker_pull gcr.io/google_containers/skydns:2015-10-13-8c72f8c
+  docker_pull gcr.io/google_containers/kube-ui:v3
+  docker_pull gcr.io/google_containers/exechealthz:1.0
+  docker_pull gcr.io/google_containers/kube2sky:1.11
+  docker_pull gcr.io/google_containers/kubernetes-dashboard-amd64:v1.0.0
+  echoSuccess "Docker images pulled successfully!"
+
+  pushd ${docker_image_dir} > /dev/null
+  echoBold "Saving docker images to disk..."
+  docker_save quay.io/devops/docker-registry:latest docker-registry.latest.tar.gz
+  docker_save gcr.io/google_containers/etcd:2.0.9 etcd.2.0.9.tar.gz
+  docker_save gcr.io/google_containers/kube-registry-proxy:0.3 kube-registry-proxy.0.3.tar.gz
+  docker_save gcr.io/google_containers/pause:2.0 pause.2.0.tar.gz
+  docker_save quay.io/coreos/flannel:0.5.5 flannel.0.5.5.tar.gz
+  docker_save gcr.io/google_containers/skydns:2015-10-13-8c72f8c skydns.2015.10.tar.gz
+  docker_save gcr.io/google_containers/kube-ui:v3 kube-ui.v3.tar.gz
+  docker_save gcr.io/google_containers/exechealthz:1.0 exechealthz.1.0.tar.gz
+  docker_save gcr.io/google_containers/kube2sky:1.11 kube2sky.1.11.tar.gz
+  docker_save gcr.io/google_containers/kubernetes-dashboard-amd64:v1.0.0 kubernetes-dashboard.1.0.0.tar.gz
+  echoSuccess "Docker images saved to disk successfully!"
+  popd > /dev/null
+fi
+
+echoBold "Starting vagrant setup..."
 eval "$* vagrant up"
 
-echo "Deploying kubernetes UI..."
+echoBold "Deploying kubernetes UI..."
 kubectl create -f plugins/kube-ui/ --namespace=kube-system
 
-echo "Deploying kubernetes dashboard..."
+echoBold "Deploying kubernetes dashboard..."
 kubectl create -f plugins/kubernetes-dashboard/ --namespace=kube-system
 
-#echo "Stopping http server..."
-#pkill http-server.py || true
 duration=$SECONDS
-echo "Kubernetes cluster started in $(($duration / 60)) minutes and $(($duration % 60)) seconds"
+echoSuccess "Kubernetes cluster started in $(($duration / 60)) minutes and $(($duration % 60)) seconds"
